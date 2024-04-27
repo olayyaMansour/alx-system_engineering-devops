27ee27163a6e7537f2e37768549f9fbb70c30f55
olayyaMansour <mansour.olayya@gmail.com>
2024-04-27 13:49:58 -0700

    add task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to install and configure Nginx web server
+
 # Install Nginx package
 package { 'nginx':
   ensure => installed,
@@ -10,10 +12,32 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
+# Nginx default page template
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
 # Nginx server configuration
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
   content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+# Nginx default server listens on port 80
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  notify  => Service['nginx'],
+}
+
+# Ensure Nginx returns "Hello World!" at root /
+file_line { 'nginx_default_content':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => '        root /var/www/html;',
+  require => File['/etc/nginx/sites-available/default'],
   notify  => Service['nginx'],
 }
 
@@ -31,22 +55,12 @@ file { '/etc/nginx/sites-enabled/redirect':
   notify => Service['nginx'],
 }
 
-# Template for Nginx default configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
-}
-
-# Nginx default page template
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
 # Restart Nginx when the default config or redirect config changes
 exec { 'restart_nginx':
-  command => '/usr/sbin/service nginx restart',
+  command     => '/usr/sbin/service nginx restart',
   refreshonly => true,
-  subscribe => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/sites-available/redirect']],
+  subscribe   => [
+    File['/etc/nginx/sites-available/default'],
+    File['/etc/nginx/sites-available/redirect'],
+  ],
 }
