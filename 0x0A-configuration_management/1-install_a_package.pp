09d1a81b50e4605c16234623d0ac314997fcf1a0
olayyaMansour <mansour.olayya@gmail.com>
2024-04-28 06:26:25 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,8 @@
-# Puppet manifest to install Flask package
+# Puppet manifest to install Flask package using pip3
 
 # Install Flask package using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
